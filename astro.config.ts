39c1b8385b9986d0dc2f5543bbaf4049ccb3bbc5
Thomas Klein <tuomasklein@gmail.com>
2023-11-01 16:20:25 +0100

    feat: markdown can now access dependency stats

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 
+import { remarkDependencyData } from "./src/remarkPluginDependencyData";
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
     markdown: {
+        remarkPlugins: [remarkDependencyData],
         shikiConfig: {
             theme: "rose-pine-moon"
         }
