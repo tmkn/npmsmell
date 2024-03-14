6a3e8a5d11ab9cc5089a054c169e681090e09f6b
Thomas Klein <tmkn@users.noreply.github.com>
2024-03-14 23:15:09 +0100

    wip

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,20 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from "astro:db";
+
+const Author = defineTable({
+    columns: {
+        id: column.number({ primaryKey: true }),
+        name: column.text()
+    }
+});
+
+const Comment = defineTable({
+    columns: {
+        authorId: column.number({ references: () => Author.columns.id }),
+        body: column.text()
+    }
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+    tables: { Author, Comment }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,26 @@
-import { db } from 'astro:db';
+import { db, Comment, Author } from "astro:db";
+import { getCollection } from "astro:content";
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+    const allDependencies = await getCollection("dependencies");
+
+    allDependencies.forEach((dependency, i) => {
+        console.log(i, dependency.id);
+    });
+
+    // TODO
+    console.log("Seeding the database");
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    console.log("Seeding complete");
+
+    await db.insert(Author).values([
+        { id: 1, name: "Astro 2" },
+        { id: 2, name: "DB" }
+    ]);
+
+    await db.insert(Comment).values([
+        { authorId: 1, body: "Hope you like Astro DB!!" },
+        { authorId: 2, body: "Enjoy!" }
+    ]);
 }
